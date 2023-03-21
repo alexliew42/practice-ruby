3782eb2c0e1e175450d288d6ae4b52fd0302b548
Alex Liew <liewtwoo42@gmail.com>
2023-03-21 15:25:12 -0400

    print greenonion sentnce

--- store_item.rb
+++ store_item.rb
@@ -2,3 +2,4 @@ green_onions = {:color => "Green", :price => "$2.50", :taste => "earthy"}
 cotton_candy = {:color => "Blue", :price => "$0.50", :taste => "sweet"}
 french_fries = {:color => "Yellow", :price => "$5.00", :taste => "salty"}
 
+p "The green onions are #{green_onions[:color]}, who would have thought."
