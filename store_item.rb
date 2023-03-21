1b98cc6d88aee64b205c51327aaa7b21d0bd8a2f
Alex Liew <liewtwoo42@gmail.com>
2023-03-21 15:20:15 -0400

    add french_fries hash

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,4 @@
 green_onions = {:color => "Green", :price => "$2.50", :taste => "earthy"}
 cotton_candy = {:color => "Blue", :price => "$0.50", :taste => "sweet"}
+french_fries = {:color => "Yellow", :price => "$5.00", :taste => "salty"}
 
