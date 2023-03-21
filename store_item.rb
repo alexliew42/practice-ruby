b7662884252485fb0f0166ebcfbdea25c870dfba
Alex Liew <liewtwoo42@gmail.com>
2023-03-21 15:28:08 -0400

    add print statement on taste and price of green onions

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,4 @@ cotton_candy = {:color => "Blue", :price => "$0.50", :taste => "sweet"}
 french_fries = {:color => "Yellow", :price => "$5.00", :taste => "salty"}
 
 p "The green onions are #{green_onions[:color]}, who would have thought."
+p "They cost #{green_onions[:price]} today and taste rather #{green_onions[:taste]}."
