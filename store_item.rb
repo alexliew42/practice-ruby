83de9613c1621409b25beb5f3c7b9e1309f1e085
Alex Liew <liewtwoo42@gmail.com>
2023-03-22 14:03:35 -0400

    add reader and writer attributes, change number of arguments in initialize

--- store_item.rb
+++ store_item.rb
@@ -9,44 +9,43 @@
 
 
 class Food
-  def initialize(input_color, input_price, input_taste)
-    @color = input_color
-    @price = input_price
-    @taste = input_taste
-  end
+  attr_reader :color, :price, :taste
+  attr_writer :price
 
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
-  def price=(new_price)
-    @price = new_price
-  end
-
-  def taste
-    @taste
+  def initialize(input)
+    @color = input[:color]
+    @price = input[:price]
+    @taste = input[:taste]
   end
 end
 
-green_onions = Food.new("Green", "$2.50", "Earthy")
+green_onions = Food.new(
+  {
+    color: "Green", 
+    price: "$2.50", 
+    taste: "Earthy"
+  }
+)
 p green_onions
 p green_onions.color
 p green_onions.price
 p green_onions.taste
-cotton_candy = Food.new("Blue", "$0.50", "Sweet")
+cotton_candy = Food.new(
+  {
+    color: "Blue", 
+    price: "$0.50", 
+    taste: "Sweet"
+  }
+)
 p cotton_candy
 p cotton_candy.color
 p cotton_candy.price
 p cotton_candy.taste
-french_fries = Food.new("Yellow", "$5.00", "Salty")
-p french_fries
-p french_fries.color
-p french_fries.price
-p french_fries.taste
+# french_fries = Food.new("Yellow", "$5.00", "Salty")
+# p french_fries
+# p french_fries.color
+# p french_fries.price
+# p french_fries.taste
 
-french_fries.price=("$2.50")
-p french_fries.price
+# french_fries.price=("$2.50")
+# p french_fries.price
