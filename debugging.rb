3432e3f81ff635928e2369dafc6d540a1888185a
Alex Liew <liewtwoo42@gmail.com>
2023-03-23 16:10:45 -0400

    create practice_file.rb

---
+++ debugging.rb
@@ -0,0 +1,14 @@
+dickens = ["Charles Dickens", "1870"]
+thackeray = ["William Thackeray”, “1863"]
+trollope = ["Anthony Trollope", "1882"]
+hopkins = [{"Gerard Manley Hopkins" => "1889"}]
+def died(array)
+  name = array[0]
+  year = array[1]
+  puts  "#{name} died in #{year}."
+end
+
+puts died(dickens)
+puts died(thackeray)
+puts died(trollope)
+puts died(hopkins)
